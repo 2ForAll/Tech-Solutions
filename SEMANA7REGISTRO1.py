d971574bba0310c4b9889296ecb1adf435f40aa8
2forall <lucasmosca2205@gmail.com>
2024-06-05 23:49:33 -0300

    Codigo teste de interacao com numeros

---
+++ SEMANA7REGISTRO1.py
@@ -0,0 +1,7 @@
+numero = float(input("Digite um numero: "))
+if numero > 0:
+  print("O numero digitado é positivo")
+elif numero < 0:
+    print("O numero digitado é negativo")
+else:
+    print("O numero digitado é igual a zero")
